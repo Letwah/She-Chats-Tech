cc14e5ff713c00db3f9e2da96188e4945745790d
Colette <hello@colettesmith.com>
2024-01-14 15:16:10 +0000

    attempt at shaders animation -tbc

--- src/pages/Home.jsx/index.jsx
+++ src/pages/Home.jsx/index.jsx
@@ -1,7 +1,9 @@
 import { ReactSVG } from "react-svg";
+// import ThreeComp from "../../components/ThreeComp";
 const Home = () => {
   return (
     <>
+      {/* <ThreeComp /> */}
       <ReactSVG src="src/assets/images/logo.svg" />
     </>
   );
